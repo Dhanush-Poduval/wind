39356ad35cf6e4b50ba0797474beeeb35b509e77
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-02-27 19:45:29 +0530

    feat:creating config to add a potentiometer to change glow of led

--- adc_bindings/src/main.c
+++ adc_bindings/src/main.c
@@ -8,12 +8,18 @@ static const int32_t sleep_time_ms=1000;
 #define MY_ADC_CH DT_ALIAS(my_adc_channel)
 static const struct device *adc=DEVICE_DT_GET(DT_ALIAS(my_adc));
 static const struct adc_channel_cfg adc_ch=ADC_CHANNEL_CFG_DT(MY_ADC_CH);
-
+static const struct gpio_dt_spec led=GPIO_DT_SPEC_GET(DT_ALIAS(my_led),gpios);
 int main(){
   int ret;
   uint16_t buf;
   uint16_t raw_val;
   int16_t  vref_mv;
+  uint16_t status;
+  int res;
+  if (res<0){
+    return 0;
+  }
+  res=gpio_pin_configure_dt(&led,GPIO_OUTPUT_ACTIVE);
   vref_mv=DT_PROP(MY_ADC_CH,zephyr_vref_mv);
   struct adc_sequence seq={
    .channels=BIT(adc_ch.channel_id),
