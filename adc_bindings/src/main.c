c1ec49f19a2bc44eee351dc805d64d32bfd1ce64
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-02-27 23:05:05 +0530

    feat:added the correct device tree in main code now mapping

--- adc_bindings/src/main.c
+++ adc_bindings/src/main.c
@@ -6,8 +6,12 @@
 static const int32_t sleep_time_ms=1000;
 
 #define MY_ADC_CH DT_ALIAS(my_adc_channel)
+#define LIGH_ADC_CH DT_ALIAS(ligh_control_channel)
+//static const struct adc_dt_spec adc_light=ADC_DT_SPEC_GET(DT_ALIAS(light-control-channel));
+//static const struct adc_dt_spec adc_main=ADC_DT_SPEC_GET(DT_ALIAS(my_adc_channel));
 static const struct device *adc=DEVICE_DT_GET(DT_ALIAS(my_adc));
 static const struct adc_channel_cfg adc_ch=ADC_CHANNEL_CFG_DT(MY_ADC_CH);
+static const struct adc_channel_cfg light_adc=ADC_CHANNEL_CFG(LIGHT_ADC_CH);
 static const struct gpio_dt_spec led=GPIO_DT_SPEC_GET(DT_ALIAS(my_led),gpios);
 int main(){
   int ret;
@@ -20,12 +24,12 @@ int main(){
     return 0;
   }
   res=gpio_pin_configure_dt(&led,GPIO_OUTPUT_ACTIVE);
-  vref_mv=DT_PROP(MY_ADC_CH,zephyr_vref_mv);
+  vref_mv=DT_PROP(LIGHT_ADC_CH,zephyr_vref_mv);
   struct adc_sequence seq={
-   .channels=BIT(adc_ch.channel_id),
+   .channels=BIT(light_adc.channel_id),
    .buffer=&buf,
    .buffer_size=sizeof(buf),
-   .resolution=DT_PROP(MY_ADC_CH,zephyr_resolution),
+   .resolution=DT_PROP(LIGHT_ADC_CH,zephyr_resolution),
   };
   if(!device_is_ready(adc)){
     printk("ADC NOT SET /READY \n");
@@ -38,6 +42,7 @@ int main(){
      printk("Could not set up the adc");
      continue;
    }
+
    raw_val=buf*vref_mv/(1<<seq.resolution);
    printk("Raw values : %u",buf);
    k_msleep(sleep_time_ms);
