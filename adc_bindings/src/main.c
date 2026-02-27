63fea03101889605c9899bb930904c8fcab021d8
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-02-28 01:21:11 +0530

    feat:made the code to change light intensity from potentiometer mapping not working

--- adc_bindings/src/main.c
+++ adc_bindings/src/main.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/adc.h>
+#include <zephyr/drivers/pwm.h>
 
-
-static const int32_t sleep_time_ms=1000;
+static const int32_t sleep_time_ms=100;
 
 #define MY_ADC_CH DT_ALIAS(my_adc_channel)
-#define LIGH_ADC_CH DT_ALIAS(ligh_control_channel)
+#define LIGHT_ADC_CH DT_ALIAS(light_control_channel)
 //static const struct adc_dt_spec adc_light=ADC_DT_SPEC_GET(DT_ALIAS(light-control-channel));
 //static const struct adc_dt_spec adc_main=ADC_DT_SPEC_GET(DT_ALIAS(my_adc_channel));
 static const struct device *adc=DEVICE_DT_GET(DT_ALIAS(my_adc));
 static const struct adc_channel_cfg adc_ch=ADC_CHANNEL_CFG_DT(MY_ADC_CH);
-static const struct adc_channel_cfg light_adc=ADC_CHANNEL_CFG(LIGHT_ADC_CH);
-static const struct gpio_dt_spec led=GPIO_DT_SPEC_GET(DT_ALIAS(my_led),gpios);
+static const struct adc_channel_cfg light_adc=ADC_CHANNEL_CFG_DT(LIGHT_ADC_CH);
+static const struct pwm_dt_spec led=PWM_DT_SPEC_GET(DT_ALIAS(my_led));
+
+int mapping(int old , int old_min , int new_max , int new_min , int old_max){
+   int new_value;
+   new_value=(((old-old_min)*(new_max-new_min))/(old_max-old_min))+new_min;
+
+};
+
 int main(){
   int ret;
   uint16_t buf;
   uint16_t raw_val;
   int16_t  vref_mv;
   uint16_t status;
-  int res;
-  if (res<0){
-    return 0;
-  }
-  res=gpio_pin_configure_dt(&led,GPIO_OUTPUT_ACTIVE);
+  int pulse;
+  int o_max;
+  int o_min=0;
+  int n_max=255;
+  int n_min=0;
+  uint16_t curr;
   vref_mv=DT_PROP(LIGHT_ADC_CH,zephyr_vref_mv);
   struct adc_sequence seq={
    .channels=BIT(light_adc.channel_id),
@@ -42,11 +50,13 @@ int main(){
      printk("Could not set up the adc");
      continue;
    }
-
-   raw_val=buf*vref_mv/(1<<seq.resolution);
-   printk("Raw values : %u",buf);
+   o_max=(1<<seq.resolution)-1;  
+   pulse=((uint64_t)buf*led.period)/4095;
+   printk("Raw values : %u\n",buf);
+   printk("Mapped values : %u\n",pulse);
+   pwm_set_pulse_dt(&led,pulse);
    k_msleep(sleep_time_ms);
-  }
-
+  };
+  return 0;
 
 }
