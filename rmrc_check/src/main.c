6b959b3dadcd994d0a18f3dc3a4b66fccffdfe63
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-03-09 20:23:06 +0530

    feat:successfully building the whole files and changed overlay and cmake files

--- rmrc_check/src/main.c
+++ rmrc_check/src/main.c
@@ -4,16 +4,16 @@
 
 
 
-#define sleep_time_ms=1000;
+#define sleep_time_ms 1000 
 
-static const struct gpio_dt_spec led=GPIO_DT_SPEC_GET(DT_ALIAS(yellow_led),gpios);
+static const struct gpio_dt_spec led=GPIO_DT_SPEC_GET(DT_ALIAS(my_led),gpios);
 
 
 int main() {
   int ret ;
   int status;
   status=1;
-  ret=gpio_pin_dt_configure(&led,GPIO_OUTPUT_ACTIVE);
+  ret=gpio_pin_configure_dt(&led,GPIO_OUTPUT_ACTIVE);
   gpio_pin_set_dt(&led,1);
   if(ret<0){
     printk("Device led not configured proeprly");
