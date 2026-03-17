1fe726bf5e1b1818b96d138569d442c91775b326
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-03-17 20:23:00 +0530

    feat:fixed the dts file and prj and main file properly for stm32

--- counters/src/main.c
+++ counters/src/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include <zpehyr/drivers/counter.h>
+#include <zephyr/drivers/counter.h>
 
 #define COUNTER_DELAY_US 1000000
 #define ALARM_CH_ID 0
-static const struct gpio_dt_spec led =GPIO_DT_SPEC_GET(DT_ALIAS(led67),gpios);
+static const struct gpio_dt_spec led =GPIO_DT_SPEC_GET(DT_ALIAS(my_led),gpios);
 void counter_isr(const struct device *dev,uint8_t chan_id,uint32_t ticks,void *user_data){
   struct counter_alarm_cfg *alarm_cfg =user_data;
   uint8_t status;
   alarm_cfg->ticks=counter_us_to_ticks(dev,COUNTER_DELAY_US);
   counter_set_channel_alarm(dev,ALARM_CH_ID,alarm_cfg);
   while(1){
-    gpio_pin_toggle(&led67);
+    gpio_pin_toggle_dt(&led);
   };
 }
 
@@ -38,7 +38,7 @@ int main(void){
   }
   alarm_ret=counter_set_channel_alarm(counter_dev,ALARM_CH_ID,&alarm_cfg);
   printk("%d\n",alarm_cfg.ticks);
-  whiel(1){
+  while(1){
     k_sleep(K_FOREVER);
   };
 
