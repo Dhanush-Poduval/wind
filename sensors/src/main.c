3f2f6e8a5e06019ec66ac2602ae5c25b6313da7a
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-03-20 03:40:48 +0530

    Fix.the array pointers in the funciton and printing for better unerstanding

--- sensors/src/main.c
+++ sensors/src/main.c
@@ -6,17 +6,16 @@
 #define sleep_time_ms 1000
 
 static const struct gpio_dt_spec led =GPIO_DT_SPEC_GET(DT_ALIAS(my_led),gpios);
-static const struct device *mpu_sensor =DEVICE_DT_GET(DT_ALIAS(turntable));
+static const struct device *mpu_sensor =DEVICE_DT_GET(DT_ALIAS(turntable_imu));
 struct sensor_value accel[3] , gyro[3];
 int main(){
   int ret;
   int sensor_ret;
-  uint8_t status;
   if(!device_is_ready(mpu_sensor)){
     printk("MPu sensor is not ready and configured properly \n");
     return 0;
   }
-  ret=gpio_configure_dt(&led,GPIO_OUTPUT_ACTIVE);
+  ret=gpio_pin_configure_dt(&led,GPIO_OUTPUT_ACTIVE);
   if(ret<0){
     printk("Not able to congiure the led pins \n");
   }
@@ -26,12 +25,12 @@ int main(){
       printk("Error reading values from the sensor not configured properly \n");
       return 0;
     }
-    sensor_ret=sensor_channel_get(mpu_sensor,SENSOR_CHAN_ACCEL_XYZ,&accel);
-    sensor_ret=sensor_channel_get(mpu_sensor,SENSOR_CHAN_GYRO_XYZ,&gyro);
+    sensor_ret=sensor_channel_get(mpu_sensor,SENSOR_CHAN_ACCEL_XYZ,accel);
+    sensor_ret=sensor_channel_get(mpu_sensor,SENSOR_CHAN_GYRO_XYZ,gyro);
     //can add for temperature also
-    printk("(x , y , z , roll , pitch , yaw ) : %d , %d , %d ,%d , %d , %d \n",sensor_value_to_double(&accel[0]),sensor_value_to_double(&accel[1]),sensor_value_to_double(&accel[2]),sensor_value_to_double(&gyro[0]),sensor_value_to_double(&gyro[1]),sensor_value_to_double(&gyro[2]));
-    led_ret=gpio_pin_toggle(&led);
-    k_sleep(sleep_time_ms);
+    printk("Accel : %f , %f , %f , Gyro : %f , %f , %f \n",sensor_value_to_double(&accel[0]),sensor_value_to_double(&accel[1]),sensor_value_to_double(&accel[2]),sensor_value_to_double(&gyro[0]),sensor_value_to_double(&gyro[1]),sensor_value_to_double(&gyro[2]));
+    gpio_pin_toggle_dt(&led);
+    k_msleep(sleep_time_ms);
 
   };
   
