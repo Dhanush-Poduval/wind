d4e719883afe83ae79d59a23662989819a13ecac
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-04-10 02:32:23 +0530

    feat:made code to read values of the first sensor

--- bio_sensors/src/main.c
+++ bio_sensors/src/main.c
@@ -7,8 +7,8 @@
 
 static const uint8_t sleep_time_ms =1000;
 
-#define ADC_CHANNEL_1 DT_ALIAS(sensors14_channel);
-#define ADC_CHANNEL_2 DT_ALIAS(sensors5_channel);
+#define ADC_CHANNEL_1 DT_ALIAS(sensors14_channel)
+#define ADC_CHANNEL_2 DT_ALIAS(sensors5_channel)
 static const struct adc_channel_cfg adc_channel_1=ADC_CHANNEL_CFG_DT(ADC_CHANNEL_1);
 static const struct adc_channel_cfg adc_channel_2=ADC_CHANNEL_CFG_DT(ADC_CHANNEL_2);
 static const struct device *bio_sen1=DEVICE_DT_GET(DT_ALIAS(bio_sensor1));
@@ -19,7 +19,7 @@ static const struct device *bio_sen5=DEVICE_DT_GET(DT_ALIAS(bio_sensor5));
 
 int main(){
   int ret;
-  uint8_t buf;
+  uint16_t buf;
   uint8_t status;
   uint8_t vref;
   uint8_t vref2;
@@ -31,6 +31,24 @@ int main(){
     .buffer_size=sizeof(buf),
     .resolution=DT_PROP(ADC_CHANNEL_1,zephyr_resolution),
   };
+  struct adc_sequence seq2={
+    .channels=BIT(adc_channel_2.channel_id),
+    .buffer=&buf,
+    .buffer_size=sizeof(buf),
+    .resolution=DT_PROP(ADC_CHANNEL_2,zephyr_resolution),
+  };
+  if(!device_is_ready(bio_sen1)){
+    printk("Bio sensor 1 not configured properly \n");
+  };
+  ret=adc_channel_setup(adc,&adc_channel_2);
+  while(1){
+    ret=adc_read(bio_sen1,&seq);
+    if(ret<0){
+      printk("Error in reading value of sensor 1");
+      return;
+    }
+    printk("The value read by the sensor is : %u\n",buf);
+  }
   
 
 }
