d3c3ed55b4dd617e29ef06fdae42dbcd5fe572b2
Dhanush-Poduval <dhanushpoduval@gmail.com>
2026-04-12 01:00:42 +0530

    feat:successfuly built the code

--- linear_actuators/src/main.c
+++ linear_actuators/src/main.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<zephyr/kernel.h>
-#include<zehyr/drivers/pwm.h>
+#include<zephyr/drivers/pwm.h>
 #include<zephyr/drivers/gpio.h>
 
 const int sleep_time_ms=1000;
 
-static const struct pwm_dt_spec *link1=PWM_DT_SPEC_GET(DT_ALIAS(link_1),pwms);
-static const struct pwm_dt_spec *link2=PWM_DEVICE_DT_SPEC_GET(DT_ALIAS(link_2),pwms);
-uint16_t maxpulse=PWM_DEVICE_DT_SPEC_GET(DT_ALIAS(link_2),max_pulse);
-uint16_t minpulse=PWM_DEVICE_DT_SPEC_GET(DT_ALIAS(link_1),min_pulse);
+static const struct pwm_dt_spec link1=PWM_DT_SPEC_GET(DT_ALIAS(link_1));
+static const struct pwm_dt_spec link2=PWM_DT_SPEC_GET(DT_ALIAS(link_2));
+uint16_t maxpulse=DT_PROP(DT_ALIAS(link_1),max_pulse);
+uint16_t minpulse=DT_PROP(DT_ALIAS(link_1),min_pulse);
 
 #define STEP PWM_USEC(100)
 
@@ -22,7 +22,7 @@ int main(){
   enum direction dir=UP;
   int ret;
   if (!pwm_is_ready_dt(&link1) && !pwm_is_ready_dt(&link2)) {
-		printk("Error: PWM device is not ready\n",);
+		printk("Error: PWM device is not ready\n");
 		return 0;
 	};
   while(1){
@@ -30,9 +30,9 @@ int main(){
     if(dir==DOWN){
       if(pulse_width<minpulse){
         dir=UP;
-        pulse_width=min_pulse;
+        pulse_width=minpulse;
       } else {
-        pulse_widt-=STEP;
+        pulse_width-=STEP;
       };
     } else {
       pulse_width+=STEP;
